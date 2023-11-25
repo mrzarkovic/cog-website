6b60ef5d328d4deea85ca94a3bc25e2f7e595601
Milos Zarkovic <milos.zarkovic@holywally.com>
2023-11-25 01:56:36 +0100

    homepage update and about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,60 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                "gradient-x": "gradient-x 15s ease infinite",
+                "gradient-y": "gradient-y 15s ease infinite",
+                "gradient-xy": "gradient-xy 15s ease infinite",
+            },
+            keyframes: {
+                "gradient-y": {
+                    "0%, 100%": {
+                        "background-size": "400% 400%",
+                        "background-position": "center top",
+                    },
+                    "50%": {
+                        "background-size": "200% 200%",
+                        "background-position": "center center",
+                    },
+                },
+                "gradient-x": {
+                    "0%, 100%": {
+                        "background-size": "200% 200%",
+                        "background-position": "left center",
+                    },
+                    "50%": {
+                        "background-size": "200% 200%",
+                        "background-position": "right center",
+                    },
+                },
+                "gradient-xy": {
+                    "0%, 100%": {
+                        "background-size": "400% 400%",
+                        "background-position": "left center",
+                    },
+                    "50%": {
+                        "background-size": "200% 200%",
+                        "background-position": "right center",
+                    },
+                },
+            },
+        },
     },
-    plugins: [require("@tailwindcss/typography"), require("daisyui")],
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("daisyui"),
+        function ({ addComponents }) {
+            addComponents({
+                ".hide-line-number": {
+                    paddingLeft: "1.25rem",
+                },
+                ".hide-line-number::before": {
+                    display: "none !important",
+                },
+            });
+        },
+    ],
     daisyui: {
         themes: ["sunset"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
         darkTheme: false, // name of one of the included themes for dark mode
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ export default {
     root: "./src",
     base: "",
     build: {
+        rollupOptions: {
+            input: {
+                main: "./src/index.html",
+                about: "./src/about.html",
+            },
+        },
         outDir: "../docs",
     },
 };
